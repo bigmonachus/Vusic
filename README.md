ba44078f8cdacc6941707f65d86814cb552f8bec
Sergio Gonzalez <bigmonachus@gmail.com>
2013-09-28 21:30:56 -0500

    glfw3 window!

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 VUSIC
 =====
+
+
+Instrs
+------
+(reqs: glfw3 shared)
+cd glfw-rs
+rustpkg build glfw
+rustpkg install glfw
+cp -r lib ../lib
--- main.rs
+++ main.rs
@@ -1,3 +1,16 @@
+extern mod glfw;
+
 fn main () -> () {
+    do glfw::set_error_callback |_, desc| {
+        print(fmt!("GLFW error: %s", desc));
+    }
     println("Hello world");
+    do glfw::start {
+        let window = glfw::Window::create(640, 480, "Holy shit this works", glfw::Windowed).unwrap();
+        window.make_context_current();
+        while !window.should_close() {
+            window.swap_buffers();
+            glfw::poll_events();
+        }
+    }
 }
